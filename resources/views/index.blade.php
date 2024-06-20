8fd976867cab7c2a4cbd23365ef0694522c0a04f
UJ <sukach.eugene@gmail.com>
2024-06-20 17:35:11 +0300

    first routing and blade template are creating

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<h1>The list of tasks</h1>
+
+<div>
+    <!-- @if(count($tasks)) -->
+    @forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">
+            {{ $task->title }}
+        </a>
+    </div>
+    @empty
+    <div>No tasks yet</div>
+    @endforelse
+    <!-- @endif -->
+</div>
