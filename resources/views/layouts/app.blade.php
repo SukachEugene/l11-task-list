190b42c7965db632a23a3b34cb2ef7aa3b446233
UJ <sukach.eugene@gmail.com>
2024-06-25 12:36:41 +0300

    added task creating form with validation, error messages and flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,18 @@
 
 <head>
     <title>Laravel 11 Task List APP</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
-    <div>@yield('content')</div>
+    <div>
+        @if (session()->has('success'))
+        <div>{{ session('success') }}</div>
+        @endif
+        
+        @yield('content')
+    </div>
 </body>
 
 </html>
